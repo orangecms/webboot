7fbb88b5ef23449a28ac5827dae51a9151b3247b
Daniel Maslowski <info@orangecms.org>
2020-01-29 01:44:21 +0100

    run-webboot.sh: add -machine q35 and use rtl8139 netdev
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- run-webboot.sh
+++ run-webboot.sh
@@ -12,7 +12,7 @@ qemu-system-x86_64 \
   -m $RAM_SIZE \
   -object rng-random,filename=/dev/urandom,id=rng0 \
   -device virtio-rng-pci,rng=rng0 \
-  -netdev user,id=u1 -device e1000,netdev=u1 \
+  -netdev user,id=network0 -device rtl8139,netdev=network0 \
   -kernel $KERNEL \
   -initrd $INITRD \
   -append "console=ttyS0 vga=786 memmap=$PMEM_SIZE!$PMEM_SIZE" \
