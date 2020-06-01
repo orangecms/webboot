f732dd3968396bedf4d6df2b1b968b78a96cb26d
Daniel Maslowski <info@orangecms.org>
2020-06-01 20:10:59 +0200

    Adjust run-webboot.sh
    * 4GB pmem size
    * 6GB for VM
    * e1000 NIC
    * use KVM
    
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- run-webboot.sh
+++ run-webboot.sh
@@ -2,17 +2,17 @@
 
 # https://docs.pmem.io/persistent-memory/getting-started-guide/creating-development-environments/virtualization/qemu
 # PMEM *must* be less than RAM so the system has some memory to work with
-export PMEM_SIZE=1G
-export RAM_SIZE=4G
+export PMEM_SIZE=4G
+export RAM_SIZE=6G
 export KERNEL=$1
 export INITRD=$2
 
 qemu-system-x86_64 \
-  -machine q35 \
+  -machine q35,accel=kvm \
   -m $RAM_SIZE \
   -object rng-random,filename=/dev/urandom,id=rng0 \
   -device virtio-rng-pci,rng=rng0 \
-  -netdev user,id=network0 -device rtl8139,netdev=network0 \
+  -netdev user,id=network0 -device e1000,netdev=network0 \
   -kernel $KERNEL \
   -initrd $INITRD \
   -append "console=ttyS0 vga=786 memmap=$PMEM_SIZE!$PMEM_SIZE" \
